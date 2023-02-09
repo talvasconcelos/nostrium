3d64a81ca2283a516e8293ad0d57e55abd496c7c
fiatjaf <fiatjaf@gmail.com>
2023-02-09 15:56:20 -0300

    giant update.

---
+++ src/Index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { link } from "svelte-spa-router";
+</script>
+
+<div>
+  <p>Nostrium is a <a href="https://github.com/nostr-protocol/nips/blob/master/23.md">NIP-23</a> article reader client.</p>
+  <p>Some distinguished authors:</p>
+  <ul>
+    <li>
+      <a use:link href="/npub13a56exdeda7y44vtnrxr3ljaxh8q9kh04e73vzw8jl8rknuj7h7sst4t9m">talvasconcelos</a>
+    </li>
+    <li>
+      <a use:link href="/nprofile1qqsw3dy8cpumpanud9dwd3xz254y0uu2m739x0x9jf4a9sgzjshaedcprdmhxue69uhkummnw3ez6ur4vgh8xetdd9ek7mpwv3jhvqgawaehxw309ahx7um5wgkhqatz9emk2mrvdaexgetj9ehx2aqpr3mhxue69uhkummnw3ez6un9d3shjtn4de6x2argwghx6egsqstvg">yak</a>
+    </li>
+  </ul>
+</div>
